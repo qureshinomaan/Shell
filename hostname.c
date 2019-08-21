50d951f548aca952a5a79dcf03456dff2ba4d3b4
qureshi.nomaan <nqafzal@gmail.com>
2019-08-22 03:26:14 +0530

    First commit

---
+++ hostname.c
@@ -0,0 +1,32 @@
+#include<dirent.h>
+#include<stdio.h>
+#include<string.h>
+#include<sys/wait.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<time.h>
+#include<sys/types.h>
+
+struct stat statRes;
+struct rusage usage;
+char command[100],cpy_cmd[100];
+char home[100],pwd[100],dir[100],user[256],host[256];
+int id;
+
+void ls();
+void hme();
+void pd();
+void cd();
+void username();
+void vi();
+void echo();
+void showpwd();
+void printEveryTime();
+void pinfo();
+
+extern void hostname()
+{
+	if(gethostname(host,256)!= 0)
+		perror("");
+}
