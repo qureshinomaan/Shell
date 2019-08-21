12087ff564a8d88762be85d07b127b04af9d6fda
qureshi.nomaan <nqafzal@gmail.com>
2019-08-22 03:25:42 +0530

    First commit

---
+++ cd.c
@@ -0,0 +1,34 @@
+#include<dirent.h>
+#include<stdio.h>
+#include<string.h>
+#include<sys/wait.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<time.h>
+#include<sys/types.h>
+
+struct stat statRes;
+struct rusage usage;
+char command[100],cpy_cmd[100];
+char home[100],pwd[100],dir[100],user[256],host[256];
+int id;
+
+void ls();
+void hme();
+void pd();
+void username();
+void vi();
+void hostname();
+void echo();
+void showpwd();
+void printEveryTime();
+void pinfo();
+
+extern void cd()
+{
+	int i;
+	char actual[100];
+	if(chdir(dir)!=0)
+		perror("cd ");
+}
