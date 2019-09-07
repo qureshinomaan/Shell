9466a18b78c9df04b57eb63d9a71c2d83fb89ccf
qureshi.nomaan <nqafzal@gmail.com>
2019-09-07 11:25:30 +0530

    Testing out stuff

--- fg.c
+++ fg.c
@@ -35,7 +35,7 @@ extern void fg(char *argv[], int len)
         printf("Could not get PGID for stdin.n");
         return(EXIT_FAILURE);
      }
-	else if (tcsetpgrp(STDIN_FILENO, stdin_PGID) == -1) {
+	else if (tcsetpgrp(STDIN_FILENO, getpid()) == -1) {
         printf("Could not set PGID.n");
         return(EXIT_FAILURE);
      }
