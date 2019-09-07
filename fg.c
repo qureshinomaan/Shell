d42f9f7d3672913602acb4e784043985049879a5
qureshi.nomaan <nqafzal@gmail.com>
2019-09-07 08:56:55 +0530

    Testing out stuff

--- fg.c
+++ fg.c
@@ -23,7 +23,7 @@ extern void fg(char *argv[], int len)
 	if(len!=2)
 		printf("Wrong Number of Arguments!\n");
 	pid_t pid= atoi(argv[1]);
-	kill(pid, SIGCONT);
+	kill(pid, 9);
 	waitpid(-1, &status, WUNTRACED );
 	return;
 }
