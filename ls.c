0ab008d62b088626fc6a32c6659d5da7c7ffbefa
qureshi.nomaan <nqafzal@gmail.com>
2019-09-13 05:10:55 +0530

    Regular Commit

--- ls.c
+++ ls.c
@@ -10,11 +10,19 @@
 #include<sys/resource.h>
 #include<sys/types.h>
 
+struct dirent *ent;
+
 struct stat statRes;
 struct rusage usage;
 char command[100],cpy_cmd[100];
 char home[100],pwd[100],dir[100],user[256],host[256];
 int id;
+char input_file[100], output_file[100]; 
+int inputD, outputD;
+int current_fg;
+int amIPiped[100] , isNxtPiped[100] ;
+int piping[100][2];
+int cmdcnt;
 
 void hme();
 void pd();
@@ -68,6 +76,66 @@ void fileProperties(struct stat stats)
  
 extern void ls(char actual_cmd[])
 {
+
+	pid_t cpid;
+	int *stat;
+	int status;
+	//printf("bhow\n");
+    pid_t cid = fork();
+    if(cid==0)
+    {  
+	//============================================================//
+    	// Input, Output redirection starts here. 
+	//============================================================//
+		int fdin,fdout;
+		if(inputD == 1)
+		{
+			fdin = open(input_file, O_RDWR);
+			dup2(fdin, 0);
+			close(fdin);
+		}
+		if(outputD == 1 || outputD == 2)
+		{
+			// You must give at least one of O_WRONLY, O_RDONLY, O_RDWR
+			if(outputD == 2)
+				fdout = open(output_file,O_APPEND | O_WRONLY);
+			else 
+				fdout = open(output_file,O_WRONLY);
+			if(fdout == -1)
+			{
+				mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+				fdout = creat(output_file, mode);
+				close(fdout);
+				fdout = open(output_file,O_RDWR);
+			}
+			dup2(fdout, 1);
+			close(fdout);
+		}
+		//============================================================//
+    	// Input, Output redirection ends here. 
+		//============================================================//
+
+		//============================================================//
+    	// Piping starts here. 
+		//============================================================//
+
+		if(amIPiped[cmdcnt] == 1)
+		{
+			dup2(piping[cmdcnt-1][0], 0);
+			close(piping[cmdcnt-1][1]);
+			close(piping[cmdcnt-1][0]);
+		}
+		if(isNxtPiped[cmdcnt] == 1)
+		{
+			dup2(piping[cmdcnt][1], 1);
+			close(piping[cmdcnt][0]);
+			close(piping[cmdcnt][1]);
+		}
+
+		//============================================================//
+    	// Piping ends here. 
+		//============================================================//
+		
 	DIR *dir;
 	int l=0,a=0;
 	int i=0;
@@ -95,33 +163,34 @@ extern void ls(char actual_cmd[])
 		else if(cpy_cmd[i-1] == ' ' && cpy_cmd[i]!='\n')
 			isdir =1;
 	}
- 	struct dirent *entry;
+
+ 	//
 	if ((dir = opendir(pwd)) == NULL )
 		perror("opendir() error");
 	else {		
-	    while ((entry = readdir(dir)) != NULL)
+	    while ((ent = readdir(dir)) != NULL)
 	      {
 	      	if(a==0)
 	      	{
-	      		if(entry->d_name[0]!='.')
+	      		if(ent->d_name[0]!='.')
 	      		{
 	      			if(isdir==0)
 	      			{
-	      				printf("  %10s ", entry->d_name);
+	      				printf("  %10s ", ent->d_name);
 	      			     if(l==1)
 	      				 {
-	      				    stat(entry->d_name,&statRes);
-	      				    fileProperties(statRes);
+	      				    // stat(ent->d_name,&statRes);
+	      				    // fileProperties(statRes);
 	      				  }
 	      				printf("\n");
 	      			}
 	      			else if(isdir == 1)
 	      			{
-	      				printf("  %10s ", entry->d_name);
+	      				printf("  %10s ", ent->d_name);
 	      			     if(l==1)
 	      				 {
-	      				    stat(entry->d_name,&statRes);
-	      				    fileProperties(statRes);
+	      				    // stat(ent->d_name,&statRes);
+	      				    // fileProperties(statRes);
 	      				  }
 	      				printf("\n");
 	      			}
@@ -131,26 +200,28 @@ extern void ls(char actual_cmd[])
 	  		{
 	  			if(isdir == 0 )
 	  			{	
-		  			printf("  %10s ", entry->d_name);
+		  			printf("  %10s ", ent->d_name);
 		      		if(l==1)
 		      		{
-		      			stat(entry->d_name,&statRes);
-		      			fileProperties(statRes);
+		      			// stat(ent->d_name,&statRes);
+		      			// fileProperties(statRes);
 		      		}
 		      		printf("\n");
 	  			}
 	  			else if(isdir==1)
 	  			{
-	  				printf("  %10s ", entry->d_name);
+	  				printf("  %10s ", ent->d_name);
 		      		if(l==1)
 		      		{
-		      			stat(entry->d_name,&statRes);
-		      			fileProperties(statRes);
+		      			// stat(ent->d_name,&statRes);
+		      			// fileProperties(statRes);
 		      		}
 		      		printf("\n");
 	  			}
 	  		}
 	      }
 	    closedir(dir);
+	}
+	_exit(0);
 	}
 }
