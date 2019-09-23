78b6a2179ea09bc0b7edc199b9f95c0df910abd1
qureshi.nomaan <nqafzal@gmail.com>
2019-09-23 16:59:00 +0530

    jobs

--- fg.c
+++ fg.c
@@ -22,9 +22,9 @@ extern void fg(char *argv[], int len)
 	int status, error;
 	if(len!=2)
 		printf("Wrong Number of Arguments!\n");
-	int jobno= atoi(argv[1]);
-	pid_t pid = pidlst[jobno-1][1];
-
+	//int jobno= atoi(argv[1]);
+	//pid_t pid = pidlst[jobno-1][1];
+	pid_t pid = atoi(argv[1]);
 	if(getpgid(pid)!= getpid())
 		tcsetpgrp(0, pid);
 	kill(pid, SIGCONT);
